2a5fa923ab86be96064bbd5e8fd175b92f41d0aa
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-04-13 19:48:14 -0300

    Metade feita

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -8,23 +8,40 @@
     <title>{{ $title }}</title> 
 </head>
 <body>
-    <header>
+    <header class="header">
         <div class="container">
-            <div class="header-logo">
-                <h1>{{ $title }}</h1>
+            <div class="container__nav">
+                <div class="header__logo">
+                    <h1>{{ $title }}</h1>
+                </div>
+                <nav class="nav">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#about" class="nav__link">Sobre</a></li>
+                        <li class="nav__item"><a href="#services" class="nav__link">Serviços Oferecidos</a></li>
+                        <li class="nav__item"><a href="#mission" class="nav__link">Missão</a></li>
+                        <li class="nav__item"><a href="#vision" class="nav__link">Visão</a></li>
+                        <li class="nav__item"><a href="#values" class="nav__link">Valores</a></li>
+                    </ul>
+                </nav>
             </div>
-            <nav class="desck">
-                <ul>
-                    <li><a href="#about">Sobre</a></li>
-                    <li><a href="#services">Serviços Oferecidos</a></li>
-                    <li><a href="#mission">Missão</a></li>
-                    <li><a href="#vision">Visão</a></li>
-                    <li><a href="#values">Valores</a></li>
-                </ul>
-            </nav>
+            <div class="cta">
+                <h2 class="cta__title">Precisa de Consultoria em Telecomunicações?</h2>
+                <p class="cta__description">Descubra como podemos otimizar seus serviços de telecomunicações com uma avaliação gratuita. Aproveite esta oportunidade exclusiva para receber insights valiosos sobre como melhorar a eficiência e a qualidade dos seus serviços. Agende sua avaliação agora mesmo e dê o primeiro passo para alcançar o sucesso em sua empresa de telecomunicações!</p>
+                <a href="#" class="cta__button">Entre em Contato</a>
+            </div>            
         </div>
     </header>
-    {{ $slot }}
+
+    <section class="about">
+        <div class="container">
+            <div class="title">
+                <h1>Conheça nossa empresa</h1>
+                <div class="title__line"></div>
+            </div>
+            
+        </div>
+    </section>
     
+    {{ $slot }}
 </body>
 </html>
