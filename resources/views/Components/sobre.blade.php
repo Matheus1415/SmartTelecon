96ee797e4ecad4413dcc516322c7b57753bb6238
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-05-02 10:19:26 -0300

    css

--- resources/views/Components/sobre.blade.php
+++ resources/views/Components/sobre.blade.php
@@ -1,6 +1,8 @@
 <div class="sobre__single">
-    <h3 class="sobre__single-title">{{$title}} 
-        <i class="{{$ico}}"></i>
-    </h3>
-    <p class="sobre__single-description">{{$descricao}}</p>
+    <div class="sobre__single__container">
+        <h3 class="sobre__single-title">{{$title}}
+            <i class="{{$ico}}"></i>
+        </h3>
+        <p class="sobre__single-description">{{$descricao}}</p>
+    </div>
 </div>
