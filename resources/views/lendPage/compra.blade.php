aa3f5864d1ef1330c23ec9fd98ea273b962def27
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-05-15 13:05:37 -0300

    Pagamento concluido

--- resources/views/lendPage/compra.blade.php
+++ resources/views/lendPage/compra.blade.php
@@ -1,12 +1,94 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Compra deo planod e internet</title>
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+    <link rel="stylesheet" href="{{asset('css/compra.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet"/>
 </head>
 <body>
-    <h1>Fassa a comra do seu plano</h1>
+    <div class="container">
+        <div class="boxDetalhe">
+            <h2>Fassa sua compra agora <span class="destaque">Teste Mode</span></h2>
+            <div class="asinatura">
+                <p class="asinatura_nome">Assine o plano Plus</p>
+                <h1>R$ 230,00 <span>por mês</span></h1>
+            </div>
+            <div class="sectionPlano">
+                <div class="sectionPlano_detalhe">
+                    <p>Plano Plus</p>
+                    <p> R$ 230,00</p>
+                </div>
+                <p class="sectionPlano_paragrafo">Cobrado mensalmente</p>
+            </div>
+
+            <div class="sectionPlano">
+                <div class="sectionPlano_detalhe">
+                    <p>Sub Total</p>
+                    <p> R$ 0,00</p>
+                </div>
+                <a href="#">Coloque o codego Promocional</a>
+            </div>
+            <div class="sectionPlano">
+                <div class="sectionPlano_detalhe">
+                    <p>Total da Compra</p>
+                    <p> R$ 0,00</p>
+                </div>
+            </div>
+        </div>
+        <div class="boxCompra">
+            <form action="">
+                @csrf
+                <div class="form_wraper">
+                    <h2>Dados de Contato</h2>
+                    <div class="form_wraper_input">
+                        <label for="emailCompra ">E-mail</label>
+                        <input type="email" id="emailCompra" name="emailCompra" placeholder="Ex:. exemplo@gamil.com">
+                    </div>
+                </div>
+
+                <div class="form_wraper">
+                    <h2>Forma de Pagamento</h2>
+                    <div class="form_flex_pagemento">
+                        <div class="boxPagemento">
+                            <i class="ri-bank-card-fill"></i>
+                            <p>Cartão</p>
+                        </div>
+                        <div class="boxPagemento">
+                            <i class="ri-bank-card-2-fill"></i>
+                            <p>Boleto</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form_wraper">
+                    <h2>Dados do Cartão</h2>
+                    <div class="form_flex">
+                        <input type="text" id="numeroCartao" name="numeroCartao" placeholder="Ex:. 1234 1234 1234 1234">
+                        <img src="imagem/formaDePagemento.png" alt="">
+                    </div>
+                    <div class="form_flex">
+                        <div class="form_flex_wraper">
+                            <input type="text" name="" id="" placeholder=" MM/AA">
+                        </div>
+                        <div class="form_flex_wraper">
+                            <input type="text" name="" id="" placeholder=" CVC">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form_wraper">
+                    <h2>Nome do Titular</h2>
+                    <input type="text" id="nomeCompra" name="nomeCompra" placeholder="Digite seu nome">
+                </div>
+
+                <input type="submit" value="Comprar Plano">
+
+            </form>
+        </div>
+    </div>
 </body>
 </html>
